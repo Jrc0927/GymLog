7eea854f1f12606bc389e8b1a632d0db90a35bb1
Joshua <https://github.com/Jrc0927>
2025-04-15 15:42:21 -0700

    Made login screen use database. usertable not full? not tracking admin1/testuser1?

--- app/src/main/java/com/example/gymlog/database/GymLogRepository.java
+++ app/src/main/java/com/example/gymlog/database/GymLogRepository.java
@@ -77,4 +77,19 @@ public void insertUser(User... user){
         });
     }
 
+    public User getUserByUserName(String username) {
+        Future<User> future = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<User>(){
+                    @Override
+                    public User call() throws Exception {
+                        return userDAO.getUserByUserName(username);
+                    }
+                });
+        try{
+            future.get();
+        }catch(InterruptedException | ExecutionException e){
+            Log.i(MainActivity.TAG, "Problem when getting all user by username");
+        }
+        return null;
+    }
 }
